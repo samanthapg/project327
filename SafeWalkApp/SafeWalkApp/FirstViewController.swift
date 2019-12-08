e82be85b57949cfd43d5323934a8511f7afb6147
samanthapg <47875003+samanthapg@users.noreply.github.com>
2019-12-08 17:51:15 -0500

    saving user input

--- SafeWalkApp/SafeWalkApp/FirstViewController.swift
+++ SafeWalkApp/SafeWalkApp/FirstViewController.swift
@@ -10,31 +10,41 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    
     @IBOutlet weak var NameField: UITextField!
-    
-    
     @IBOutlet weak var LastNameField: UITextField!
-    
-    
     @IBOutlet weak var AddressField: UITextField!
-    
     @IBOutlet weak var TextView: UITextView!
+    
+    let defaults = UserDefaults.standard
+    
+    struct Keys {
+        static let name = "Name"
+        static let lastname = "Last Name"
+        static let address = "Address"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        NameField.delegate = self
-        LastNameField.delegate = self
-        AddressField.delegate = self
+       // NameField.delegate = self
+        checkForSavedName()
+        checkForSavedLastName()
+        checkForSavedAddress()
+        saveNamePreference()
+        saveLastNamePreference()
+        saveAddressPreference()
+       // LastNameField.delegate = self
+       // AddressField.delegate = self
         // Do any additional setup after loading the view.
-        
         
     }
 
 
     @IBAction func EnterPressed(_ sender: Any) {
-        TextView.text = "Name: \(NameField.text!)\n LastName: \(LastNameField.text!)\n Address:\(AddressField.text!)\n"
+       // TextView.text = "Name: \(NameField.text!)\n LastName: \(LastNameField.text!)\n Address:\(AddressField.text!)\n"
+        
+        saveNamePreference()
+        saveLastNamePreference()
+        saveAddressPreference()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -44,11 +54,40 @@ class FirstViewController: UIViewController {
 
     }
     
+    func saveNamePreference(){
+        defaults.set(NameField.text!, forKey: Keys.name)
+       // defaults.set(LastNameField, forKey: Keys.lastname)
     }
-extension FirstViewController : UITextFieldDelegate {
+    
+    func saveLastNamePreference(){
+        defaults.set(LastNameField.text!, forKey: Keys.lastname)
+    }
+    
+    func saveAddressPreference(){
+        defaults.set(AddressField.text!, forKey: Keys.address)
+    }
+    
+    func checkForSavedName() {
+        let name = defaults.value(forKey: Keys.name)as? String ?? ""
+        NameField.text = name
+    }
+    
+    func checkForSavedLastName(){
+        let lastname = defaults.value(forKey: Keys.lastname)as? String ?? ""
+        LastNameField.text = lastname
+    }
+    
+    func checkForSavedAddress(){
+        let address = defaults.value(forKey: Keys.address)as? String ?? ""
+        AddressField.text = address
+    }
+    
+    
+    }
+/*extension FirstViewController : UITextFieldDelegate {
     func textFieldShouldReturn( _ textField: UITextField)->Bool {
     textField.resignFirstResponder()
-    return true
-}
-}
+    return true*/
+//}
+//}
 
