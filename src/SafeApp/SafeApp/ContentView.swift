5eee98428fdb6c079f6cc8e1aeb086894aa9fec7
samanthapg <47875003+samanthapg@users.noreply.github.com>
2019-12-11 18:29:41 -0500

    Everything User Interface still have to check message

---
+++ src/SafeApp/SafeApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SafeApp
+//
+//  Created by Samantha Puterman on 12/7/19.
+//  Copyright © 2019 Samantha Puterman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      //  Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+*/
