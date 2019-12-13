c4f347f8c39c798d04c36f4dfe1c57a793dab541
alepineiro <55723646+alepineiro@users.noreply.github.com>
2019-12-13 17:21:36 -0500

    Add files via upload

---
+++ src/SafeWalk/README.md
@@ -0,0 +1,36 @@
+# project327
+Pikashu!!!
+
+- Byron Mitchell
+- Jean Marc Achkar
+- Samantha Puterman
+- ALexandra Pineiro
+
+Project Overvew:
+
+Our project consisted of making an IOS app that allowed users to get the safest route back to their homes. The targetted audience for this app is mainly women, college students, and tourists that are in a foreign country. The app would prompt the user for their name, and address, and the user would be able to see on a map the directions and the ETA to their destination. We also have a feature that asks the user every 5 minutes if they are okay, if the user fails to reply to that pop up notif for a certain amount of time, the app will automatically contact the police and share the user's location with them. The app also includes a Notify the police button in case the user is unable to speak on the phone.
+
+How to 'build' project:
+
+First - Xcode must be downloaded to your device
+
+Second - Download the latest .zip file of SafeWalk.zip and extract it
+
+Third - In the folder open the SafeWalk.xcodeproj
+
+Fourth - Xcode should load, then hit run to execute the simulator
+
+Fifth - Enjoy our App!
+
+Note - To appropiately run our app and get the notifying a person or authority feature, an 
+        Iphone with the latest IOS Software Uptade is needed. 
+
+How we built the project:
+
+Since we are designing an IOS app, the language we decided to go with is Swift, and we are designing our project on Xcode. Xcode is one of the best IDEs to develop IOS apps. By taking advantage of all the design features, it is easier for us to direcly use the story board features which allows us to add occurences on the app layout and connect them to the code easily. This came in handy when we implemented the MapKit feature that gave us access to the Apple Maps IPA. 
+
+The group was divided into two, one that focuses on the user inteface and the other on the map. After each group completed their respective goals and code, the next step was to implement the map design with the UI and make our tabbed application function accordingly.
+
+Link to youtube video: 
+
+Extras:
---
+++ src/SafeWalk/src/SafeWalk /SafeWalk/FirstViewController.swift
@@ -0,0 +1,109 @@
+//
+//  FirstViewController.swift
+//  SafeWalk
+//
+//  Created by Byron Mitchell on 12/11/19.
+//  Copyright © 2019 Byron Mitchell. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+
+class FirstViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    
+    
+
+    @IBOutlet weak var NameField: UITextField!
+    @IBOutlet weak var LastNameField: UITextField!
+    @IBOutlet weak var AddressField: UITextField!
+    
+    let defaults = UserDefaults.standard
+    
+    struct Keys {
+        static let name = "Name"
+        static let lastname = "Last Name"
+        static let address = "Address"
+        static let phoneNumber = 7873793060;
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        checkForSavedName()
+        checkForSavedLastName()
+        checkForSavedAddress()
+        saveNamePreference()
+        saveLastNamePreference()
+        saveAddressPreference()
+    }
+
+    @IBAction func SavePressed(_ sender: Any) {
+        saveNamePreference()
+        saveLastNamePreference()
+        saveAddressPreference()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        NameField.resignFirstResponder()
+        LastNameField.resignFirstResponder()
+        AddressField.resignFirstResponder()
+    }
+    
+    func saveNamePreference(){
+        defaults.set(NameField.text!, forKey: Keys.name)
+    }
+    
+    func saveLastNamePreference(){
+        defaults.set(LastNameField.text!, forKey: Keys.lastname)
+    }
+    
+    func saveAddressPreference(){
+        defaults.set(AddressField.text!, forKey: Keys.address)
+    }
+    
+    func checkForSavedName(){
+        let name = defaults.value(forKey: Keys.name)as? String ?? ""
+        NameField.text = name
+    }
+    
+    func checkForSavedLastName(){
+        let lastname = defaults.value(forKey: Keys.lastname)as?
+        String ?? ""
+        LastNameField.text = lastname
+    }
+    
+    func checkForSavedAddress(){
+        let address = defaults.value(forKey: Keys.address)as? String ?? ""
+        AddressField.text = address
+    }
+    
+    
+    @IBOutlet weak var Contactingauthorities: UITextField!
+    @IBAction func HelpButtonPressed(_ sender: Any) {
+        Contactingauthorities.isHidden = false
+        displayMessageInterface()
+        }
+    
+    func displayMessageInterface() {
+        let composeVC = MFMessageComposeViewController()
+        composeVC.messageComposeDelegate = self
+        
+        // Configure the fields of the interface.
+        composeVC.recipients = ["7873793060"]
+        composeVC.body = "Message sent using the SafeWalk app: It's Samantha Puterman, I am walking back home and I don't feel safe"
+        
+        // Present the view controller modally.
+        if MFMessageComposeViewController.canSendText() {
+            self.present(composeVC, animated: true, completion: nil)
+        } else {
+            print("Can't send messages.")
+        }
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+    }
+}
+    
+
